73892d9eddb1a225ad2aeb03c2a0d78f78da5825
Renato Lacerda <renato.ac.lacerda@gmail.com>
2025-03-09 18:55:52 -0300

    chore(test): testing basic collection api

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,18 @@
 import { describe, expect, it } from "vitest";
-import {} from "../src";
+import { damasco } from "../src";
 
-describe("packageName", () => {
-  it.todo("pass", () => {
-    expect(true).toBe(true);
+function createDamascoOnMemory() {
+  return damasco({ url: ":memory:" });
+}
+
+describe("damasco", () => {
+  it("creates a collection and writes to it", async () => {
+    const db = createDamascoOnMemory();
+    const collection = db.collection("myCollection");
+
+    await collection.add({ name: "John", age: 30 });
+    const data = await collection.get();
+
+    expect(data).toEqual([{ _uid: expect.any(String), name: "John", age: 30 }]);
   });
 });
